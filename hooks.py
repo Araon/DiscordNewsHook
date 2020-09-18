99c3c184b5de1b8c90aed6b2698d44299d2637fc
Ara0n <soumik.ghosh1998@gmail.com>
2020-09-18 15:21:10 +0530

    Set to Corn Schedule

--- hooks.py
+++ hooks.py
@@ -3,11 +3,11 @@ from inshorts import getNews
 import json
 import time
 
-#urlLole = "https://discord.com/api/webhooks/756417999971287071/ZXrh-fJIpapJlDg2wjv4RZvDbh-9QReINIhKK5frjB3MmsjLfql-N1AIym9fTiNxCcwP"
+urlLole = "https://discord.com/api/webhooks/756417999971287071/ZXrh-fJIpapJlDg2wjv4RZvDbh-9QReINIhKK5frjB3MmsjLfql-N1AIym9fTiNxCcwP"
 
-testUrl = "https://discord.com/api/webhooks/756397412784013402/pg6hyvthWE8-U1zyu3v3Hv1evbx8r36VMajXi3MpZK8Hlb-Uw5PmgEVSuvI0IUKuz7m-"
+#testUrl = "https://discord.com/api/webhooks/756397412784013402/pg6hyvthWE8-U1zyu3v3Hv1evbx8r36VMajXi3MpZK8Hlb-Uw5PmgEVSuvI0IUKuz7m-"
 
-hook = Webhook(testUrl)
+hook = Webhook(urlLole)
 
 newz = getNews("")
 
@@ -17,10 +17,9 @@ for bits in newz['data']:
 	newsTitle = bits['title']
 	newsContent = bits['content']
 	imageUrl = bits['imageUrl']
-	newsTime = bits['time']
-	newsDate = bits['date']
 	newsAuthor = bits['author']
 	newsReadmore = bits['readMoreUrl']
+
 	embed = Embed(
 	title=newsTitle,
 	url=newsReadmore,
@@ -36,6 +35,6 @@ for bits in newz['data']:
 	hook.send(embed=embed)
 	time.sleep(2)
 
-hook.send('**Daily News at 8am**')
+hook.send('**Daily News at 8am and 8pm**')
 
 
