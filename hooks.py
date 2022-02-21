f33b80bd9bfe3d3b2fc98266a85d28482d7e760a
araon <soumik.ghosh1998@gmail.com>
2022-02-21 12:57:49 +0530

    fixed webhook url

--- hooks.py
+++ hooks.py
@@ -3,7 +3,7 @@ from inshorts import getNews
 import json
 import time
 
-urlLole = "https://discord.com/api/webhooks/756417999971287071/ZXrh-fJIpapJlDg2wjv4RZvDbh-9QReINIhKK5frjB3MmsjLfql-N1AIym9fTiNxCcwP"
+urlLole = "https://discord.com/api/webhooks/756417996401934399/0waN-LRyC5vHse6Tqx8Ea9aaagJFZgn4PTQQ9wQ0M-7FK8LO1CenuNE1lfUfmh71pLw9"
 
 #testUrl = "https://discord.com/api/webhooks/756397412784013402/pg6hyvthWE8-U1zyu3v3Hv1evbx8r36VMajXi3MpZK8Hlb-Uw5PmgEVSuvI0IUKuz7m-"
 
